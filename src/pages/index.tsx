f1d31fbe739a1c0b6c548ee97700d20f2321c9b0
Thomás Dixini <thomasdixini@gmail.com>
2023-08-21 21:43:12 -0300

    :sparkles: Estruturando projetos
    Começando a estrutura HTML da parte do resumo e dos projetos, e aplicando alguns efeitos CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,19 +15,19 @@ export default function Home() {
           <nav className={styles.menu}>
             <ul>
               <li className={styles.item}>
-                <a href="">
+                <a href="#">
                   <span></span>
                   <span> Experiências </span>
                 </a>
               </li>
               <li className={styles.item}>
-                <a href="">
+                <a href="#">
                   <span></span>
                   <span> Experiências </span>
                 </a>
               </li>
               <li className={styles.item}>
-                <a href="">
+                <a href="#">
                   <span></span>
                   <span> Experiências </span>
                 </a>
@@ -36,23 +36,91 @@ export default function Home() {
           </nav>
 
           <footer className={styles.footer}>
-            <a href=""> <GithubLogo size={32} weight="fill" /> </a>
-            <a href=""> <InstagramLogo size={32} weight="bold" /> </a>
-            <a href=""> <LinkedinLogo size={32} weight="fill" /> </a>
+            <a href="#a"> <GithubLogo size={32} weight="fill" /> </a>
+            <a href="#b"> <InstagramLogo size={32} weight="bold" /> </a>
+            <a href="#b"> <LinkedinLogo size={32} weight="fill" /> </a>
           </footer>
         </div>
 
-        <div className={styles.projects}>
-          <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Maxime libero molestiae nesciunt vel voluptas? Nam repellendus
-            ex exercitationem iure. Quam maxime, ut cupiditate unde repellendus
-            iure eveniet fugiat pariatur tenetur.
-          </p>
+        <section className={styles.projectsSection}>
+          <div className={styles.projectsOverflow}>
+            <div>
+              <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Maxime libero molestiae nesciunt vel voluptas? Nam repellendus
+                ex exercitationem iure. Quam maxime, ut cupiditate unde repellendus
+                iure eveniet fugiat pariatur tenetur.
+              </p>
+            </div>
 
-          <article>
+            <article className={styles.project}>
+              <a href="#project">
+                <div>
+                  <strong> Shopfy </strong>
+                  <span> Subtitle </span>
+                </div>
 
-          </article>
-        </div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tempora, illum expedita. Suscipit delectus minima pariatur aut,
+                  unde molestias sint. Nihil labore qui officiis,
+                  at quo modi delectus quibusdam ipsa repudiandae.
+                </p>
+
+                <ul>
+                  <li>React</li>
+                  <li>Next</li>
+                  <li>JavaScript</li>
+                  <li>JavaScript</li>
+                  <li>JavaScript</li>
+                </ul>
+              </a>
+            </article>
+            <article className={styles.project}>
+              <a href="https://github.com/ #seila">
+                <div>
+                  <strong> Shopfy </strong>
+                  <span> Subtitle </span>
+                </div>
+
+                <p> Alguma descrição detalhada </p>
+                <ul>
+                  <li>React</li>
+                  <li>Next</li>
+                  <li>JavaScript</li>
+                </ul>
+              </a>
+            </article>
+            <article className={styles.project}>
+              <a href="https://github.com/ #seila">
+                <div>
+                  <strong> Shopfy </strong>
+                  <span> Subtitle </span>
+                </div>
+
+                <p> Alguma descrição detalhada </p>
+                <ul>
+                  <li>React</li>
+                  <li>Next</li>
+                  <li>JavaScript</li>
+                </ul>
+              </a>
+            </article>
+            <article className={styles.project}>
+              <a href="https://github.com/ #seila">
+                <div>
+                  <strong> Shopfy </strong>
+                  <span> Subtitle </span>
+                </div>
+
+                <p> Alguma descrição detalhada </p>
+                <ul>
+                  <li>React</li>
+                  <li>Next</li>
+                  <li>JavaScript</li>
+                </ul>
+              </a>
+            </article>
+          </div>
+        </section>
       </main>
     </>
   )
